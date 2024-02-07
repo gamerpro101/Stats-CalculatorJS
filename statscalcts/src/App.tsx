da8ac605b686a5a8d9e0f877e1d7106f30494d56
gamerpro101 <scrambledhandy@gmail.com>
2024-02-06 21:05:42 -0500

    Initial setup

--- statscalcts/src/App.tsx
+++ statscalcts/src/App.tsx
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        Hello from statistic calculator</p>
         <a
           className="App-link"
           href="https://reactjs.org"
